c7b7dcc17ef1967abe4d8865f5c23c27961b8f4c
awiles808 <awiles808@gmail.com>
2020-03-03 22:20:10 -0700

    uninstalled history

--- react-503devco/src/App.js
+++ react-503devco/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { About } from './About';
 import { Projects } from './Projects';
 import { Team } from './Team';
